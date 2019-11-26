ce5a7930710ff6f1a010a9e6d9ce73c209e0520a
wildcat <letranhaitung@gmail.com>
2019-11-26 15:42:31 +0700

    Add comments for socket things

--- src/mrmathami/thegame/net/MPSocketController.java
+++ src/mrmathami/thegame/net/MPSocketController.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MPSocketController {
-    private ServerSocket serverSocket = null;
     private MPSocket socket;
     private static MPSocketController currentInstance = null;
 
@@ -15,12 +14,11 @@ public class MPSocketController {
      */
     public MPSocketController() {
         try {
-            this.serverSocket = new ServerSocket(MPConfig.DEFAULT_LISTEN_PORT);
+            ServerSocket serverSocket = new ServerSocket(MPConfig.DEFAULT_LISTEN_PORT);
             this.socket = new MPSocket(serverSocket.accept());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        currentInstance = this;
     }
 
     /**
@@ -35,25 +33,44 @@ public class MPSocketController {
             e.printStackTrace();
             throw e;
         }
-        currentInstance = this;
     }
 
     /**
-     * Create an static instance, so we don't have to use DI everywhere.
+     * Return an static instance, so we don't have to use DI everywhere.
      * @return current instance
      */
     public static MPSocketController getCurrentInstance() {
-        if (currentInstance == null) {
-            return new MPSocketController();
-        } else {
-            return currentInstance;
-        }
+        return currentInstance;
     }
 
+    /**
+     * Set the new instance.
+     * @param instance MPSocketController instance to use till next set.
+     */
+    public static void setCurrentInstance(MPSocketController instance) {
+        currentInstance = instance;
+    }
+
+    /**
+     * Check if the current instance is initialized.
+     * @return false if the currentInstance is null, true otherwise.
+     */
+    public static boolean hasInstance() {
+        return currentInstance != null;
+    }
+
+    /**
+     * Send a command to remote host from a List of Strings.
+     * @param command remote command to send.
+     */
     private void sendCommand(List<String> command) {
         this.socket.sendLine(String.join(" ", command));
     }
 
+    /**
+     * Get the next line and split it by spaces.
+     * @return List of String which is space-separated from a line received from socket.
+     */
     public List<String> getNextCommand() {
         String command = this.socket.recvLine();
         if (command.isBlank()) {
@@ -62,19 +79,39 @@ public class MPSocketController {
         return Arrays.asList(command.split(" "));
     }
 
+    /**
+     * PLACE command sender. Used to place a tower on remote host.
+     * @param type type of tower.
+     * @param posX X position on game field.
+     * @param posY Y position on game field.
+     */
     public void sendPlace(int type, double posX, double posY) {
         sendCommand(List.of("PLACE", String.format("%d", type),
                 String.format("%.0f", posX), String.format("%.0f", posY)));
     }
 
+    /**
+     * UPGRADE command sender. Used to upgrade tower on the remote host.
+     * @param posX X position on game field.
+     * @param posY Y position on game field.
+     */
     public void sendUpgrade(double posX, double posY) {
         sendCommand(List.of("UPGRADE", String.format("%.0f", posX), String.format("%.0f", posY)));
     }
 
+    /**
+     * SELL command sender. Used to sell tower on the remote host.
+     * @param posX X position on game field.
+     * @param posY Y position on game field.
+     */
     public void sendSell(double posX, double posY) {
         sendCommand(List.of("SELL", String.format("%.0f", posX), String.format("%.0f", posY)));
     }
 
+    /**
+     * STATE command sender. Used to send player state to the remote host when it changes.
+     * @param health Current health of the player.
+     */
     public void sendState(long health) {
         sendCommand(List.of("STATE", Long.toString(health)));
     }
